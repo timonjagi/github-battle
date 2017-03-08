e6a2068e8f3771c4ae8883eb21034dd0e3bf1186
Timothy Njagi <timothynjihia@gmail.com>
2017-03-08 13:35:00 +0300

    'github-battle'

---
+++ app/index.js
@@ -0,0 +1,15 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var HelloWorld = React.createClass({
+	render: function(){
+		return (
+			<div> Hello {this.props.name} </div>
+		)
+	}
+});
+
+ReactDOM.render(
+	<HelloWorld name="Timothy"/>,
+	document.getElementById('app')
+);
